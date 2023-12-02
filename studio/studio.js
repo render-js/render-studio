86b892b33300cf9dd9c234ece604ce31b3fa5695
mutian <mutian-xiangcai@outlook.com>
2023-12-02 09:43:46 +0800

    version

--- studio/studio.js
+++ studio/studio.js
@@ -1,12 +1,26 @@
 import { Component } from "render-core/class/component/component";
 import template from "./studio.html";
+import {changeApplicationTheme, changeSessionTheme} from "render-core/func/Theme";
 
 export default new Component({
     name:"studio",
     template:template,
-    mode: "insert",
+    data:{
+      change: true
+    },
+    mode: "box",
     methods:{
-        send() {
+        raw_show() {
+            if (!this.change){
+                changeSessionTheme("show");
+                this.change = true
+            }else {
+                changeSessionTheme("default");
+                this.change = false
+            }
+        },
+        tk(){
+          changeApplicationTheme("default")
         }
     }
 })
