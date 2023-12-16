ac60891c9115324ac7ec4178ee3cdb39d764bfe6
mutian <mutian-xiangcai@outlook.com>
2023-12-16 17:51:39 +0800

    version

--- studio/studio.js
+++ studio/studio.js
@@ -1,26 +1,29 @@
 import { Component } from "render-core/class/component/component";
 import template from "./studio.html";
-import {changeApplicationTheme, changeSessionTheme} from "render-core/func/Theme";
 
 export default new Component({
     name:"studio",
     template:template,
     data:{
-      change: true
-    },
-    mode: "box",
-    methods:{
-        raw_show() {
-            if (!this.change){
-                changeSessionTheme("show");
-                this.change = true
-            }else {
-                changeSessionTheme("default");
-                this.change = false
-            }
+        change: true,
+        hello: "color:red",
+        ok: "hello world",
+        person:{
+            name:"liuzhang",
+            age:23,
+            sex:"male",
+            position:[
+                "one","two","three"
+            ]
         },
-        tk(){
-          changeApplicationTheme("default")
+        table:{
+            header:["name","age","sex"],
+            rows:[
+                ["liuzhang",23,"male"],
+                ["mutian",24,"female"],
+                ["huangting",24,"female"]
+            ]
         }
-    }
+    },
+    mode: "box"
 })
